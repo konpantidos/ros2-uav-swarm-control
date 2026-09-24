b6e4f94b638a674d212bf2a29c1d1a71a75be5b5
konpantidos <kon.pantidos@gmail.com>
2026-09-24 11:36:06 +0300

    Update swarm_controller.hpp

--- swarm_control/include/swarm_control/swarm_controller.hpp
+++ swarm_control/include/swarm_control/swarm_controller.hpp
@@ -5,18 +5,17 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <swarm_interfaces/msg/drone_state.hpp>
 #include <Eigen/Dense>
+#include <vector>
 
 class SwarmController : public rclcpp::Node {
 public:
     SwarmController();
 
 private:
-    // ROS 2 Communication
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_pub_;
     rclcpp::Subscription<swarm_interfaces::msg::DroneState>::SharedPtr state_sub_;
     rclcpp::TimerBase::SharedPtr control_timer_;
 
-    // Mathematical State (Eigen Vectors for pure math operations)
     Eigen::Vector3d current_position_;
     Eigen::Vector3d current_velocity_;
     Eigen::Vector3d target_position_;
@@ -24,13 +23,17 @@ private:
     // APF Control Parameters
     double k_attractive_;
     double max_velocity_;
+    
+    // Repulsive Force Parameters
+    double k_repulsive_;          
+    double influence_radius_;     
+    std::vector<Eigen::Vector3d> obstacles_; 
 
-    // Callbacks & Core Logic
     void state_callback(const swarm_interfaces::msg::DroneState::SharedPtr msg);
     void compute_and_publish_velocity();
     
-    // Math function for APF
     Eigen::Vector3d calculate_attractive_force();
+    Eigen::Vector3d calculate_repulsive_force(); 
 };
 
 #endif // SWARM_CONTROLLER_HPP
